9a265fe7df9b30dc676ee86fa235ea9204a6c061
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-09-12 09:08:57 +0200

    ajout de la fonction de configuration de socket

--- webserver/socket.c
+++ webserver/socket.c
@@ -10,4 +10,16 @@ int createServerSocket(){
     perror("socket_serveur");
   }
   /*utilisation de la socket serveur */
+
 }
+
+void confServerSocket(int socket_serveur) {
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET; /* Socket ipv4 */
+  saddr.sin_port = htons(8080); /*Port d'écoute */
+  saddr.sin_addr.s_addr = INADDR_ANY; /*écoute sur toutes les interfaces */
+
+  if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
+    perror("bind socket_serveur");
+    /* traitement de l'erreur */
+  }
