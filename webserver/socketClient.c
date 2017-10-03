ec643586223f24e32d52a4c093cfb947142d1025
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-10-03 11:54:44 +0200

    tag 8 (sort of)

--- webserver/socketClient.c
+++ webserver/socketClient.c
@@ -1,11 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <stdlib.h>
+
+#include "socketClient.h"
 
 int acceptConnection(int socket_serveur){
 
@@ -18,34 +12,75 @@ int acceptConnection(int socket_serveur){
     perror("accept\n");
     /*traitement d'erreur*/
   }
-    int pid = fork();
-    if (pid == 0) { /* Fils */
-      FILE *fp = fdopen(socket_client, "w+");
+  int pid = fork();
+  if (pid == 0) { /* Fils */
+    FILE *fp = fdopen(socket_client, "w+");
 
-      char message_client [512];
-
-      /* On peut maintenant dialoguer avec le client */
-      char *message_bienvenue = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn\n\nPRAISE LORD CTHULHU ^(;,;)^\n\n\0";
-      fprintf(fp, "<Pawnee>\n%s", message_bienvenue);
-    
-      if(fgets(message_client,512,fp)!=NULL) {
-	puts("Message client :\0");
-	puts(message_client);
-      }
-      fprintf(fp, "%s\n", message_client);
-
-
-      
-      fclose(fp);
-      exit(EXIT_SUCCESS);
+    char message_client [512];
+    traiterMessageClient(message_client, fp);
+    exit(EXIT_SUCCESS);
   		
-    } else { /* Pere */
-      if (close(socket_client) < 0){
-	perror(" close ");
-      }
-    
+  } else { /* Pere */
+    if (close(socket_client) < 0){
+      perror(" close ");
     }
+    
+  }
   
   
   return socket_client;
 }
+
+void traiterMessageClient(char * message_client,FILE * fp) {
+  int succes = -1, succes_http=-1;
+  int ligne = 0;
+  char * requete;
+  char * chemin;
+  char * protocole;
+  char * copie = malloc(512*sizeof(char));// malloc de 512 characteres
+  
+  while(fgets(message_client,512,fp)!=NULL) {
+      
+    strcpy(copie,message_client);//copie la string pointée par message_client dans copie
+    
+    if (ligne == 0) {	
+      requete = strtok(message_client, " ");// separe les mots dans la string
+      
+      if (strcmp(requete, "GET") == 0) { //compare la string requete a GET
+	
+	if ((chemin=strtok(NULL, " ")) != NULL && (protocole=strtok(NULL, " ")) != NULL
+	    && strtok(NULL, " ") == NULL) { //check si les 3 mots de l'entetes ne sont pas null
+
+	  succes = 0;
+	}
+      }
+	
+	  
+      if (succes != 0) {	      
+	printf("ERROR : Bad Request (%s) !\n", requete);
+	break;
+      }
+      
+     	  
+      if(strcmp(protocole,"HTTP/1.0\r\n") == 0 || strcmp(protocole,"HTTP/1.1\r\n") == 0) {
+	succes_http++;
+      }
+      else if(strcmp(protocole,"HTTP/1.0") == 0 || strcmp(protocole,"HTTP/1.1") ==0 ) {
+	fprintf(fp,"%s : 400 Bad Request\r\n", protocole);
+	break;
+      }
+      printf("%s", copie);
+    }
+      
+    ligne += 1;
+  }
+  free(copie);
+  fprintf(fp, "Connection: close\n");
+  
+  fclose(fp);
+
+
+}
+
+
+
--- webserver/socketClient.h
+++ webserver/socketClient.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+
 int acceptConnection(int socket_serveur);
+void traiterMessageClient(char * message_client,FILE * fp);
