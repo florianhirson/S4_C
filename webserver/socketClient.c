84b71175c6915627fec218dd2cc28a21da4850f0
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-10-10 09:50:10 +0200

    separation du code en methodes, verification de la requete

--- webserver/socketClient.c
+++ webserver/socketClient.c
@@ -6,7 +6,7 @@ int acceptConnection(int socket_serveur){
   int socket_client;
   printf("tentative de connexion ...\n");
   socket_client = accept(socket_serveur,NULL,NULL);
-  printf("connect� !\n");
+  printf("connecté !\n");
   
   if (socket_client == -1){
     perror("accept\n");
@@ -17,7 +17,7 @@ int acceptConnection(int socket_serveur){
     FILE *fp = fdopen(socket_client, "w+");
 
     char message_client [512];
-    traiterMessageClient(message_client, fp);
+    traiterRequeteClient(message_client, fp);
     exit(EXIT_SUCCESS);
   		
   } else { /* Pere */
@@ -31,56 +31,96 @@ int acceptConnection(int socket_serveur){
   return socket_client;
 }
 
-void traiterMessageClient(char * message_client,FILE * fp) {
-  int succes = -1, succes_http=-1;
-  int ligne = 0;
-  char * requete;
-  char * chemin;
-  char * protocole;
-  char * copie = malloc(512*sizeof(char));// malloc de 512 characteres
-  
-  while(fgets(message_client,512,fp)!=NULL) {
-      
-    strcpy(copie,message_client);//copie la string point�e par message_client dans copie
-    
-    if (ligne == 0) {	
-      requete = strtok(message_client, " ");// separe les mots dans la string
-      
-      if (strcmp(requete, "GET") == 0) { //compare la string requete a GET
-	
-	if ((chemin=strtok(NULL, " ")) != NULL && (protocole=strtok(NULL, " ")) != NULL
-	    && strtok(NULL, " ") == NULL) { //check si les 3 mots de l'entetes ne sont pas null
+/**
+   Verifie si l'entete de la requete est correct
+   0 si correct
+   -1 si requete incorrecte erreur 400 bad request
+   -2 si le chemin est incorrect erreur 404 not found
 
-	  succes = 0;
-	}
-      }
-	
-	  
-      if (succes != 0) {	      
-	printf("ERROR : Bad Request (%s) !\n", requete);
-	break;
-      }
-      
-     	  
-      if(strcmp(protocole,"HTTP/1.0\r\n") == 0 || strcmp(protocole,"HTTP/1.1\r\n") == 0) {
-	succes_http++;
-      }
-      else if(strcmp(protocole,"HTTP/1.0") == 0 || strcmp(protocole,"HTTP/1.1") ==0 ) {
-	fprintf(fp,"%s : 400 Bad Request\r\n", protocole);
-	break;
-      }
-      printf("%s", copie);
-    }
-      
-    ligne += 1;
+ **/
+int verifHeader(char* messageClient, char* cProtocole) {
+  char *requete,*chemin,*protocole;
+  
+  requete = strtok(messageClient, " ");
+  if (strcmp(requete,"GET") == 0){
+    if(! ((chemin=strtok(NULL, " ")) != NULL && (protocole=strtok(NULL, " ")) != NULL && strtok(NULL, " ") == NULL) )
+      return -1;
+  }else{
+    return -1;
   }
-  free(copie);
-  fprintf(fp, "Connection: close\n");
+
+  strcpy(cProtocole,protocole);
   
-  fclose(fp);
-
-
+  if(!(strcmp(chemin,"/")==0))
+    return -2 ;
+  
+  if(!(strcmp(protocole,"HTTP/1.0\r\n") == 0 || strcmp(protocole,"HTTP/1.1\r\n") == 0 ))
+    return -1;
+  
+  return 0;
 }
 
+/**
+   Verifie si il y a une ligne vide à la fin de la requete envoyée par le client
+   0 si ligne vide
+   -1 sinon
+**/
+int verifEndOfString(char* messageClient) {
+  if(messageClient[strlen(messageClient)-1]=='\n'&&messageClient[strlen(messageClient)-2]=='\r'){
+   return 0;
+  }
+  return -1;
+}
+
+
+void traiterRequeteClient(char * messageClient,FILE * fp) {     
+  char * messageAcceuil="Welcome to Babushka's basement ! Have some kompot kamarad ! ☭☭☭☭\n\r";
+  int succes,succesFin;
+  int ligne = 0;
+  char * cProtocole=malloc(50*sizeof(char));
+  
+  while(fgets(messageClient,512,fp)!=NULL) {  
+    if (ligne == 0) {	
+	succes=verifHeader(messageClient,cProtocole);
+	if(succes<0)
+	  break;
+      } else {
+	succesFin=verifEndOfString(messageClient);
+	if(succesFin<0)
+	  break;
+      }
+    ligne++;
+  }
+
+  if( succes<0 || succesFin<0 || messageClient[0]!='\r'||messageClient[1]!='\n'){      
+    traitementErr(fp,messageClient,succes);   
+  }  else    {
+    sendResponse (fp,200,"OK",messageAcceuil);
+  }
+
+  fclose(fp);
+}
+
+void sendResponse(FILE *client, int code, const char *reasonPhrase, const char *messageBody) {
+  fprintf(client, "HTTP/1.1 %d %s\r\n", code, reasonPhrase);
+  if(code != 200) {
+    fprintf(client, "Connection: close\r\n");
+  }
+  fprintf(client, "Content-Length: %zd\r\n\r\n%s", strlen(messageBody), messageBody);
+  printf("Content-Length: %zd\r\n\r\n%s", strlen(messageBody), messageBody);
+  fflush(client);
+}
+
+
+
+
+void traitementErr(FILE * fp, char * messageClient,int errorCode){
+  while(fgets(messageClient,512,fp)!=NULL);
+  if(errorCode<-1){
+    sendResponse (fp,404,"Not Found","Not Found\r\n");  
+  }else{
+    sendResponse (fp,400,"Bad Request","Bad request\r\n");
+  }
+}
 
 
