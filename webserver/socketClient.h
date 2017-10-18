f395cf4e74a94aeb85aae50ad7612735913b1f17
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-10-18 14:35:03 +0200

    hello world

--- webserver/socketClient.h
+++ webserver/socketClient.h
@@ -14,3 +14,15 @@ void traiterRequeteClient(char * messageClient,FILE * fp);
 void sendResponse(FILE *client, int code, const char *reasonPhrase, const char *messageBody);
 void traitementErr(FILE * fp, char * messageClient,int errorCode);
 char *fgets_or_exit (char* buffer, int size, FILE* stream);
+
+enum http_method {
+  HTTP_GET ,
+  HTTP_UNSUPPORTED ,
+};
+typedef struct
+{
+  enum http_method method ;
+  int major_version ;
+  int minor_version ;
+  char * target ;
+} http_request ;
